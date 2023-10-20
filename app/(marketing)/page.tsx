a27496184f1a1c24c4258df396997906887b871e
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2023-10-20 21:54:38 +0545

    create marketing Footer component

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Heading from "./_components/Heading";
 import Heros from "./_components/Heros";
+import Footer from "./_components/Footer";
 
 const MarketingPage = () => {
   return (
@@ -9,6 +10,7 @@ const MarketingPage = () => {
         <Heading />
         <Heros />
       </div>
+      <Footer />
     </div>
   );
 };
