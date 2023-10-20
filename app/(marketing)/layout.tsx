071c6b84ee05412e37970ea675e97ae1ff0bfda4
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2023-10-20 22:17:45 +0545

    create marketing navBar component

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "./_components/NavBar";
+
+const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full ">
+      <NavBar />
+      <main className="h-full pt-40">{children}</main>
+    </div>
+  );
+};
+
+export default MarketingLayout;
